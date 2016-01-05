db23bfeecae0ca61ef7c59292e3a4e2ec2571c68
jakenotjacob <jake.campbell91@gmail.com>
2016-01-05 02:31:07 -0700

    Chapter 1, exercise 14.

---
+++ 1/1-14.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//97,  65: a, A
+//122, 90: z, Z
+
+#define ALPH_LEN 26
+
+int main ()
+{
+    int chars[ALPH_LEN];
+    int c;
+
+    for(int i = 0; i < ALPH_LEN; i++)
+        chars[i] = 0;
+
+    while ((c = getchar()) != EOF)
+        ++chars[c-97];
+
+    for(int i = 0; i < ALPH_LEN; i++) {
+        printf("%c|", (i+97));
+        for(int t = chars[i]; t > 0; --t){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
